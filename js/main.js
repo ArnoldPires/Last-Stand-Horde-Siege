f911c39b23316b78d1b88fd1203e7ae010b791ad
ArnoldPires <arnold.pires92@gmail.com>
2023-08-07 18:24:11 -0400

    Working on the js a bit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// alert("Does this work")
+document.addEventListener("DOMContentLoaded", function() {
+  const startButton = document.getElementById("startSlaying");
+  const startScreenText = document.querySelector(".startScreenText");
+  const playerBase = document.querySelector(".playerBase");
+
+  startButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    startScreenText.style.display = "none"; // Hide the start screen text
+    playerBase.classList.remove("hidden"); // Show the game box
+  });
+});
